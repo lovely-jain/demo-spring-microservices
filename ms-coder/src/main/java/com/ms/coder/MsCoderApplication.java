d74f94cf29f56e283dca04164bf4f36556bdbb3e
lovely-jain <jainlovely450@gmail.com>
2021-12-31 17:44:01 +0530

    coder ms

--- ms-coder/src/main/java/com/ms/coder/MsCoderApplication.java
+++ ms-coder/src/main/java/com/ms/coder/MsCoderApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -13,5 +16,10 @@ public class MsCoderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MsCoderApplication.class, args);
 	}
-
+	
+	@LoadBalanced
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
--- ms-coder/src/main/java/com/ms/coder/controller/CoderController.java
+++ ms-coder/src/main/java/com/ms/coder/controller/CoderController.java
@@ -1,8 +1,32 @@
 package com.ms.coder.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CoderController {
-
+	@Autowired
+	private RestTemplate restTemplate;
+	
+	private String uri ="http://PROBLEMMS/";
+	
+	@GetMapping("/")
+	public ResponseEntity<String> getHelloMessage(@RequestParam String user ) {
+		return ResponseEntity.ok("Hello "+user);
+	}
+	
+	@GetMapping("/{user}/problems")
+	public ResponseEntity<List<String>> getUserRelatedProblems(@PathVariable String user ) {
+		List<String> problems = restTemplate.getForObject(uri+user, List.class);
+		return ResponseEntity.ok(problems);
+	}
+	
+	
 }
