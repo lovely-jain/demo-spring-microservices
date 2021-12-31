f574b6f73a6506e757cfc4ce1d92d45df90fa686
lovely-jain <jainlovely450@gmail.com>
2021-12-31 17:36:25 +0530

    added zuul

--- ms-zuul/src/main/java/com/ms/zuul/MsZuulApplication.java
+++ ms-zuul/src/main/java/com/ms/zuul/MsZuulApplication.java
@@ -2,8 +2,12 @@ package com.ms.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class MsZuulApplication {
 
 	public static void main(String[] args) {
