9f5cc2e821e4db567d161023d701f1ec3d6c2cd3
lovely-jain <jainlovely450@gmail.com>
2021-12-31 16:53:23 +0530

    added controller

---
+++ ms-coder/src/main/java/com/ms/coder/controller/CoderController.java
@@ -0,0 +1,8 @@
+package com.ms.coder.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CoderController {
+
+}
---
+++ ms-problem/src/main/java/com/ms/problem/controller/ProblemController.java
@@ -0,0 +1,8 @@
+package com.ms.problem.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProblemController {
+
+}
