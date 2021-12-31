f3c2276a8ec689a8002d67f8c4d988b6df0c1b54
lovely-jain <jainlovely450@gmail.com>
2021-12-31 16:07:55 +0530

    adding projects

---
+++ ms-zuul/src/test/java/com/ms/problem/MsProblemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.problem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProblemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
