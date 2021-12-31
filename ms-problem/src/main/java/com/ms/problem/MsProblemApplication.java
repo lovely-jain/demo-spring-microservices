3d68cae492b99d72190d508cbd26881761be894d
lovely-jain <jainlovely450@gmail.com>
2021-12-31 15:49:36 +0530

    updated app.

---
+++ ms-problem/src/main/java/com/ms/problem/MsProblemApplication.java
@@ -0,0 +1,13 @@
+package com.ms.problem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsProblemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsProblemApplication.class, args);
+	}
+
+}
