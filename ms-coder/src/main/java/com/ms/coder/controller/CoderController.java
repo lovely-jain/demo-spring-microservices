1ed530b96823b398d96810a2b46c1842d3681ba0
lovely-jain <jainlovely450@gmail.com>
2021-12-31 19:03:28 +0530

    controller class updated

--- ms-coder/src/main/java/com/ms/coder/controller/CoderController.java
+++ ms-coder/src/main/java/com/ms/coder/controller/CoderController.java
@@ -1,11 +1,16 @@
 package com.ms.coder.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -28,5 +33,13 @@ public class CoderController {
 		return ResponseEntity.ok(problems);
 	}
 	
+	@PutMapping("/{name}")
+	public ResponseEntity<List<String>> addProblem(@PathVariable String name ) {
+		HttpHeaders headers = new HttpHeaders();
+		HttpEntity<List> entity = new HttpEntity(headers);
+
+		ResponseEntity<List> problems = restTemplate.exchange(uri+name, HttpMethod.PUT, entity,  List.class);
+		return ResponseEntity.ok(problems.getBody());
+	}
 	
 }
--- ms-problem/src/main/java/com/ms/problem/controller/ProblemController.java
+++ ms-problem/src/main/java/com/ms/problem/controller/ProblemController.java
@@ -1,8 +1,35 @@
 package com.ms.problem.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ProblemController {
-
+	List<String> problems = new ArrayList<String>();
+	
+	@GetMapping("/{user}")
+	public List<String> allProblemsOfUser(@PathVariable("user") String userId) {
+		if("lovely".equals(userId)) {
+			problems.add("0-1 Knapsack");
+			problems.add("DFS");
+		}
+		else {
+			problems.add("Matrix Chain Multiplication");
+			problems.add("BFS");
+		}
+		return problems;
+	}
+	
+	@PutMapping("/{problem}")
+	public ResponseEntity<List<String>> addProblems(@PathVariable("problem") String name) {
+		problems.add(name);
+		return ResponseEntity.ok(problems);
+	}
+	
 }
